ae28609ea3dbeedb197ea48bd6b55466d2dbf355
jonaaix <jonaaix@icloud.com>
2026-01-03 01:52:52 +0100

    Fix heading margins

--- src/Support/HtmlRenderer/Renderers/HeaderRenderer.php
+++ src/Support/HtmlRenderer/Renderers/HeaderRenderer.php
@@ -23,35 +23,35 @@ class HeaderRenderer implements BlockRenderer
                 font-size: 2.5em; 
                 font-weight: 800; 
                 line-height: 1.2; 
-                margin: 2em 0 0.67em; /* Vorher 0.67em oben */
+                margin: 1.2em 0 0.6em;
             }
             
             %scope% h2 { 
                 font-size: 2em; 
                 font-weight: 700; 
                 line-height: 1.25; 
-                margin: 1.75em 0 0.75em; /* Vorher 0.75em oben */
+                margin: 1.2em 0 0.6em; 
             }
             
             %scope% h3 { 
                 font-size: 1.5em; 
                 font-weight: 700; 
                 line-height: 1.3; 
-                margin: 1.5em 0 0.83em; /* Vorher 0.83em oben */
+                margin: 1.2em 0 0.6em; 
             }
             
             %scope% h4 { 
                 font-size: 1.25em; 
                 font-weight: 600; 
                 line-height: 1.4; 
-                margin: 1.5em 0 1em; /* Vorher 1em oben */
+                margin: 1.2em 0 0.6em; 
             }
             
-            %scope% h5,
+            %scope% h5, 
             %scope% h6 { 
                 font-size: 1em; 
                 font-weight: 600; 
-                margin: 1.5em 0 1.2em; /* Vorher 1.2em oben */
+                margin: 1.2em 0 0.6em; 
             }
         CSS;
     }
